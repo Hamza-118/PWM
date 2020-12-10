0a67c4a5da74d4f3b4dba123941fae51f8900374
Hamza Ahmed Khan <60402389+Hamza-118@users.noreply.github.com>
2020-12-10 19:17:29 +0500

    oscilloscope file

---
+++ oscilloscope_file_10dec.ino
@@ -0,0 +1,19 @@
+int pwm = 11;
+float a;
+int x;
+void setup() {
+pinMode(pwm,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int duty_cycle;
+for(duty_cycle=0 ; duty_cycle<=255; duty_cycle++){
+analogWrite(pwm,duty_cycle);
+delay(100);
+x = analogRead(A0);
+a=(5/1023.0)*x;
+Serial.println(a);
+}
+}
