aefa8698f2b5b4000455b7ccb5606b270d297b58
Hamza Ahmed Khan <60402389+Hamza-118@users.noreply.github.com>
2020-12-11 17:50:51 +0500

    linear pwm actuation

---
+++ linear_pwm_oscillation.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+//analogWrite(3,OUTPUT);
+Serial.begin(115200);
+}
+
+void loop() {
+  int x=  analogRead (A0);
+  // put your main code here, to run repeatedly:
+analogWrite(3,x);
+Serial.println(x);
+delay (20);
+}
